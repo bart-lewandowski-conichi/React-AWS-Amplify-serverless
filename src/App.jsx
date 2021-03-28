d89499b25fd6606a70329b81627804421728741b
Bartosz Lewandowski <mebarlew@gmail.com>
2021-03-28 21:26:55 +0200

    authentication log in log out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+// Amplify
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+//amplify/ui
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// Css
+import "./App.css";
+
+Amplify.configure(awsExports);
 
 const App = () => {
   return (
     <>
-      <main>
-        <h1>hello world</h1>
-      </main>
+      <AmplifyAuthenticator>
+        <main>
+          <h1>hello world</h1>
+        </main>
+        <AmplifySignOut />
+      </AmplifyAuthenticator>
     </>
   );
 };
